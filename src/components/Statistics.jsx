344f70608e91598bfa5fda3e199ace58a9362969
etopritika <dimaprytyka@gmail.com>
2023-04-30 20:18:50 +0300

    init title

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -5,7 +5,7 @@ import getRandomHexColor from './color-generator';
 export default function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.stat_list}>
         {stats.map(({ id, label, percentage }) => (
           <li
